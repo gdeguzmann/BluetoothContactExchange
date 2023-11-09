361bc80d9a2ac092c410c84fe0e510452b90bf13
rordonez102 <112515391+rordonez102@users.noreply.github.com>
2023-11-09 17:58:52 -0600

    documentation

--- app/src/main/java/com/example/projectmobileappdev/data/chat/BluetoothDeviceMapper.kt
+++ app/src/main/java/com/example/projectmobileappdev/data/chat/BluetoothDeviceMapper.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import com.example.projectmobileappdev.domain.chat.BluetoothDeviceDomain
 
+//converts bluetooth device to bluetooth device domain that contains name and mac address of the device.
 @SuppressLint("MissingPermission")
 fun BluetoothDevice.toBluetoothDeviceDomain(): BluetoothDeviceDomain{
     return BluetoothDeviceDomain(
