a1755dc1cb9e079ebe6073d6c342bffbe7fafd91
rordonez102 <112515391+rordonez102@users.noreply.github.com>
2023-11-09 17:36:43 -0600

    documentation

--- app/src/main/java/com/example/projectmobileappdev/domain/chat/BluetoothController.kt
+++ app/src/main/java/com/example/projectmobileappdev/domain/chat/BluetoothController.kt
@@ -3,11 +3,12 @@ package com.example.projectmobileappdev.domain.chat
 import kotlinx.coroutines.flow.StateFlow
 
 interface BluetoothController {
+    // Stateflow that represents a list of scanned and paired bluetooth devices.
     val scannedDevices: StateFlow<List<BluetoothDevice>>
     val pairedDevices: StateFlow<List<BluetoothDevice>>
-
+    //function that starts and stops the discovery process for bluetooth devices.
     fun startDiscovery()
     fun stopDiscovery()
-
+    //function that releases any resources being used by the controller
     fun release()
 }
