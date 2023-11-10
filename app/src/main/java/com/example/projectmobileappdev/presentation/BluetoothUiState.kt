fb2b75e810f7186ed41c86acb8ef92d2e30a16c7
rordonez102 <112515391+rordonez102@users.noreply.github.com>
2023-11-09 18:08:02 -0600

    documentation

--- app/src/main/java/com/example/projectmobileappdev/presentation/BluetoothUiState.kt
+++ app/src/main/java/com/example/projectmobileappdev/presentation/BluetoothUiState.kt
@@ -1,5 +1,5 @@
 package com.example.projectmobileappdev.presentation
-
+//data class that represents the state of the scanned and paired devices.
 data class BluetoothUiState(
     val scannedDevices: List<com.example.projectmobileappdev.domain.chat.BluetoothDevice> = emptyList(),
     val pairedDevices: List<com.example.projectmobileappdev.domain.chat.BluetoothDevice> = emptyList(),
